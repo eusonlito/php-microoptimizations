7f39f04ff60c763b9ad4dd8b88fd3431da708ee1
Lito <lito@eordes.com>
2016-07-03 21:47:17 +0200

    Fixed typo

--- src/templates/molecules/header.php
+++ src/templates/molecules/header.php
@@ -1,5 +1,5 @@
 <div class="brand"><a href="<?= route('/') ?>">PHP micro-optimizations</a></div>
-<div class="address-bar">Becase the small things are important</div>
+<div class="address-bar">Because the small things are important</div>
 
 <div class="container">
     <div class="row">
