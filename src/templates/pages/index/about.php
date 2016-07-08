106ba10e4bd64423ffe3edfb2134fb3dd5b4daae
Lito <lito@eordes.com>
2016-07-08 17:36:34 +0200

    Added phpfarm information

--- src/templates/pages/index/about.php
+++ src/templates/pages/index/about.php
@@ -3,6 +3,7 @@
         <p>This is a personal project</p>
         <p>This server has 1 CPU and 1 GB RAM, results in seconds are higher than a regular server</p>
         <p>Time in seconds is an average for all tests (one daily) for each test/version</p>
+        <p>Using <a href="https://github.com/eusonlito/phpfarm" target="_blank">https://github.com/eusonlito/phpfarm</a> to perform tests</p>
         <p>More info and collaborations in <a href="https://github.com/eusonlito/php-microoptimizations" target="_blank"><strong>GitHub</strong></a></p>
     </section>
 </div>
